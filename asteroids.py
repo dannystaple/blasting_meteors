9be3df52954b473ffc8fa4fd064cab692722e202
Danny Staple <danny@orionrobots.co.uk>
2019-06-17 21:36:21 +0100

    Spaceship is more manouvrable

--- asteroids.py
+++ asteroids.py
@@ -1,28 +1,35 @@
 import random
+from pygame import Vector2
+
 HEIGHT = 600
 WIDTH = 1000
 
 spaceship = Actor('spaceship.gif')
+spaceship.speed = Vector2(0, 0)
 spaceship.x = WIDTH // 2
-spaceship.y = HEIGHT - 20
+spaceship.y = HEIGHT // 2
 spaceship.angle = 180
 
-spaceship.x_increment = 0
-spaceship.y_increment = 0
+spaceship.x_velocity = 0
+spaceship.y_velocity = 0
 spaceship.fire_direction = (0, -5)
 spaceship.fire_location = lambda: spaceship.midtop
+
+rocket = Actor('fireball')
+rocket.engine_radius = spaceship.height // 2 + rocket.height // 2
+
 score = 0
 
 asteroid = Actor("asteroid.png")
 
 def prepare_asteroid():
     global asteroid
-    if random.randint(0, 1) >= 1:
-        asteroid.x = random.randint(int(spaceship.right) + 50, WIDTH - 20)
+    if spaceship.left < WIDTH // 2:
+        asteroid.x = random.randint(max(WIDTH // 2, int(spaceship.right) + 50), WIDTH - 20)
     else:
-        asteroid.x = random.randint(20, int(spaceship.left) - 50)
+        asteroid.x = random.randint(20, min(int(spaceship.left) - 50, WIDTH // 2))
     asteroid.bottom = random.randint(asteroid.height, HEIGHT-40)
-    score_scale = (score) + 3
+    score_scale = score + 3
     asteroid.x_velocity = random.randint(-score_scale, score_scale)
     asteroid.y_velocity = random.randint(-score_scale, score_scale)
 
@@ -39,27 +46,43 @@ class Bullet(Actor):
     def update(self):
         """Update the bullet. Return true if it hit"""
         global score
-        self.x += self.x_velocity * speed_scale
-        self.y += self.y_velocity * speed_scale
+        self.x += self.speed.x
+        self.y += self.speed.y
         if asteroid and self.colliderect(asteroid):
             sounds.blast.play()
             prepare_asteroid()
-            asteroid.x = random.randint(20, WIDTH - 20)
-            asteroid.bottom = random.randint(asteroid.height, HEIGHT - 40)
-            asteroid.x_velocity = random.randint(-4, 4)
-            asteroid.y_velocity = random.randint(-4, 4)
             score += 1
             return True
         if self.x > WIDTH or self.x < 0 or self.y > HEIGHT or self.y < 0:
             return True
 
 
+def update_rocket_flare():
+    rocket_rel = Vector2()
+    rocket_rel.from_polar((rocket.engine_radius, 270 - spaceship.angle))
+    rocket.angle = spaceship.angle + 90
+    rocket.center = (spaceship.center[0] + rocket_rel.x, spaceship.center[1] + rocket_rel.y)
+
 def update():
     global playing, asteroid
     if playing:
-        spaceship.x = (spaceship.x + spaceship.x_increment * speed_scale) % WIDTH
-        spaceship.y = (spaceship.y + spaceship.y_increment * speed_scale) % HEIGHT
-        #
+        spaceship.x = (spaceship.x + spaceship.speed.x) % WIDTH
+        spaceship.y = (spaceship.y + spaceship.speed.y) % HEIGHT
+
+        if playing:
+            if keyboard.left:
+                spaceship.angle += 20
+            elif keyboard.right:
+                spaceship.angle -= 20
+            if keyboard.up:
+                new_speed = Vector2()
+                new_speed.from_polar((speed_scale, 90 - spaceship.angle))
+                spaceship.speed += new_speed
+                update_rocket_flare()
+                if spaceship.speed.length_squared() > 16:
+                    spaceship.speed.scale_to_length(4)
+                sounds.thrust.play()
+
     if asteroid:
         asteroid.x += asteroid.x_velocity * speed_scale
         asteroid.y += asteroid.y_velocity * speed_scale
@@ -84,6 +107,9 @@ def draw():
     """Redraw the screen."""
     screen.fill((0,0,0))
     spaceship.draw()
+
+    if keyboard.up:
+        rocket.draw()
     if asteroid:
         asteroid.draw()
     if not playing:
@@ -95,35 +121,16 @@ def draw():
 def on_key_down(key):
     if not playing:
         return
-    if key == keys.UP:
-        spaceship.angle = 180
-        spaceship.y_increment = -2
-        spaceship.fire_direction = (0, -5)
-        spaceship.fire_location = lambda: spaceship.midtop
-    elif key == keys.DOWN:
-        spaceship.angle = 0
-        spaceship.y_increment = 2
-        spaceship.fire_direction = (0, 5)
-        spaceship.fire_location = lambda: spaceship.midbottom
-    elif key == keys.RIGHT:
-        spaceship.angle = 90
-        spaceship.x_increment = 2
-        spaceship.fire_direction = (5, 0)
-        spaceship.fire_location = lambda: spaceship.midright
-    elif key == keys.LEFT:
-        spaceship.angle = 270
-        spaceship.x_increment = -2
-        spaceship.fire_direction = (-5, 0)
-        spaceship.fire_location = lambda: spaceship.midleft
     elif key == keys.SPACE:
         if len(bullets) < 10:
-            new_bullet = Bullet('bullet', spaceship.fire_location())
-            new_bullet.x_velocity = spaceship.fire_direction[0]
-            new_bullet.y_velocity = spaceship.fire_direction[1]
+            new_bullet = Bullet('bullet')
+            new_bullet.speed = Vector2()
+            new_bullet.speed.from_polar((5 * speed_scale, 90 - spaceship.angle))
+            new_bullet.pos = spaceship.pos + new_bullet.speed
             music.play_once("laser5")
             bullets.append(new_bullet)
 
 
 def on_key_up():
-    spaceship.x_increment = 0
-    spaceship.y_increment = 0
+    spaceship.x_velocity = 0
+    spaceship.y_velocity = 0
